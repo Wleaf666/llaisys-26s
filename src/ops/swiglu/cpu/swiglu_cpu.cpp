31b062e6fb27afd1f1ed11ef29f63eb497360d51
Wleaf666 <1617844769@qq.com>
2026-07-24 11:26:10 +0800

    complete ops

--- src/ops/swiglu/cpu/swiglu_cpu.cpp
+++ src/ops/swiglu/cpu/swiglu_cpu.cpp
@@ -1,8 +1,26 @@
 #include "swiglu_cpu.hpp"
 #include "../../../utils.hpp"
+
+template <typename T>
+void swiglu_(T* out,const T* gate,const T* up,size_t numel)
+{
+    for (size_t i = 0; i < numel;i++)
+    {
+        out[i] = llaisys::utils::cast<T>(llaisys::utils::cast<float>(up[i]) * llaisys::utils::cast<float>(gate[i]) / (1 + exp(-llaisys::utils::cast<float>(gate[i]))));
+    }
+}
+
 namespace llaisys::ops::cpu {
 void swiglu(std::byte *out, const std::byte *gate, const std::byte *up, llaisysDataType_t type, size_t numel) {
-    (void)out; (void)gate; (void)up; (void)type; (void)numel;
-    TO_BE_IMPLEMENTED();
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return swiglu_(reinterpret_cast<float *>(out), reinterpret_cast<const float *>(gate), reinterpret_cast<const float *>(up), numel);
+    case LLAISYS_DTYPE_BF16:
+        return swiglu_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const llaisys::bf16_t *>(gate), reinterpret_cast<const llaisys::bf16_t *>(up), numel);
+    case LLAISYS_DTYPE_F16:
+        return swiglu_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const llaisys::fp16_t *>(gate), reinterpret_cast<const llaisys::fp16_t *>(up), numel);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
 }
 }
